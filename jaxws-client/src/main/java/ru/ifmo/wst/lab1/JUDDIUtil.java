98603f829c56ff3216307878e07abc8d23c8a518
Ivan Uskov <liveinkarai@gmail.com>
2019-02-15 01:45:42 +0300

    Print binding templates for services.

--- jaxws-client/src/main/java/ru/ifmo/wst/lab1/JUDDIUtil.java
+++ jaxws-client/src/main/java/ru/ifmo/wst/lab1/JUDDIUtil.java
@@ -1,11 +1,13 @@
 package ru.ifmo.wst.lab1;
 
+import org.apache.juddi.api_v3.AccessPointType;
+import org.uddi.api_v3.BindingTemplate;
+import org.uddi.api_v3.BindingTemplates;
 import org.uddi.api_v3.BusinessInfo;
 import org.uddi.api_v3.BusinessInfos;
 import org.uddi.api_v3.BusinessService;
 import org.uddi.api_v3.Description;
 import org.uddi.api_v3.Name;
-import org.uddi.api_v3.ServiceInfos;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -42,6 +44,40 @@ public class JUDDIUtil {
                     .map(JUDDIUtil::nameToString)
                     .collect(Collectors.joining("\n"))
             );
+            printBindingTemplates(businessService.getBindingTemplates());
+        }
+    }
+
+    /**
+     * This function is useful for translating UDDI's somewhat complex data
+     * format to something that is more useful.
+     *
+     * @param bindingTemplates
+     */
+    private static void printBindingTemplates(BindingTemplates bindingTemplates) {
+        if (bindingTemplates == null) {
+            return;
+        }
+        for (BindingTemplate bindingTemplate : bindingTemplates.getBindingTemplate()) {
+            System.out.println("Binding Key: " + bindingTemplate.getBindingKey());
+
+            if (bindingTemplate.getAccessPoint() != null) {
+                System.out.println("Access Point: " + bindingTemplate.getAccessPoint().getValue() + " type " + bindingTemplate.getAccessPoint().getUseType());
+                if (bindingTemplate.getAccessPoint().getUseType() != null) {
+                    if (bindingTemplate.getAccessPoint().getUseType().equalsIgnoreCase(AccessPointType.END_POINT.toString())) {
+                        System.out.println("Use this access point value as an invocation endpoint.");
+                    }
+                    if (bindingTemplate.getAccessPoint().getUseType().equalsIgnoreCase(AccessPointType.BINDING_TEMPLATE.toString())) {
+                        System.out.println("Use this access point value as a reference to another binding template.");
+                    }
+                    if (bindingTemplate.getAccessPoint().getUseType().equalsIgnoreCase(AccessPointType.WSDL_DEPLOYMENT.toString())) {
+                        System.out.println("Use this access point value as a URL to a WSDL document, which presumably will have a real access point defined.");
+                    }
+                    if (bindingTemplate.getAccessPoint().getUseType().equalsIgnoreCase(AccessPointType.HOSTING_REDIRECTOR.toString())) {
+                        System.out.println("Use this access point value as an Inquiry URL of another UDDI registry, look up the same binding template there (usage varies).");
+                    }
+                }
+            }
         }
     }
 
